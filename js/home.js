7a8ac2f9af1e47a5e101c7613a6016a594074620
Ammar Ahmed <ammarahmed4517331@gmail.com>
2017-12-23 15:48:26 +0500

    completing home page

--- js/home.js
+++ js/home.js
@@ -1,11 +1,27 @@
-/**
- * Created by Ammar Ahmed on 12/18/2017.
- */
+/*** Created by Ammar Ahmed on 12/18/2017.***/
 $( document ).ready(function() {
     $('.one-time').slick({
         dots: false,
         infinite: true,
         arrows: false,
+        autoplay: false,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+    $('.one-time-partner').slick({
+        dots: true,
+        infinite: true,
+        arrows: false,
+        autoplay: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+    $('.hadith-detail-slider').slick({
+        dots: true,
+        infinite: true,
+        arrows: true,
         autoplay: true,
         speed: 300,
         slidesToShow: 1,
